cf5a156c57156736f1e0945e1c4930cf26a18c78
SergeyVolodko <volodkosm@gmail.com>
2015-07-02 00:06:01 +0300

    Upload project

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public double SpringEffectTime = 0.5;
+    private double elapsedTime = 0;
+	void Update ()
+	{
+        var spring = GetComponent<SpringJoint2D>();
+
+	    if (Input.anyKeyDown)
+        {
+            var target = GameObject.Find("MovementTarget");
+            var nextPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            target.transform.position = nextPosition;
+
+            spring.enabled = true;
+            elapsedTime = 0;
+        }
+        
+	    if (elapsedTime > SpringEffectTime)
+	    {
+	      spring.enabled = false;
+	    }
+
+	    elapsedTime += Time.deltaTime;
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+	}
+}
