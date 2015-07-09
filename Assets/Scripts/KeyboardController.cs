4e248ae848f2ccb9eb527013429aef378fb86fd1
SergeyVolodko <volodkosm@gmail.com>
2015-07-09 23:40:52 +0300

    Recommit

---
+++ Assets/Scripts/KeyboardController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class KeyboardController : MonoBehaviour
+{
+    public float ForceValue = 65f;
+    public float Gravity = 0.75f;
+
+    void Start()
+    {
+        Physics.gravity = new Vector3(0, -Gravity, 0);
+    }
+
+    void Update ()
+	{
+	    var direction = GetDirection();
+
+	    var rb = this.gameObject.GetComponent<Rigidbody>();
+        rb.AddForce(direction * ForceValue);
+    }
+
+    private static Vector3 GetDirection()
+    {
+        var direction = Vector3.zero;
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            direction = Vector3.up;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            direction = Vector3.down;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            direction = Vector3.left;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            direction = Vector3.right;
+        }
+        return direction;
+    }
+}
---
+++ Assets/Scripts/KeyboardController2dWASD.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class KeyboardController2dWASD : MonoBehaviour
+{
+    public float ForceValue = 300f;
+    
+    void Update ()
+	{
+	    var direction = GetDirection();
+
+	    var rb = this.gameObject.GetComponent<Rigidbody2D>();
+        rb.AddForce(direction * ForceValue);
+    }
+
+    private static Vector2 GetDirection()
+    {
+        var direction = Vector2.zero;
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            direction = Vector2.up;
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            direction = Vector2.down;
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            direction = Vector2.left;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            direction = Vector2.right;
+        }
+        return direction;
+    }
+}
